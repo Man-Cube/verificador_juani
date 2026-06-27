0c9cc37d077417816b3f40bd9f6b877aab16c782
Man-Cube <74361077+Man-Cube@users.noreply.github.com>
2026-06-27 14:48:42 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'verificador-juani-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Instalar el Service Worker y guardar los archivos básicos en memoria
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Hacer que la app responda rápido usando la memoria interna
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
